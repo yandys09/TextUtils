2d14d6a69ce5766085e589a1e907b33a6e213b44
yandys09 <yandys03@naver.com>
2022-03-20 17:38:06 +0900

    add button copy, remove

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -16,6 +16,17 @@ export default function TextForm(props) {
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[  ]+/)
+    setText(newText.join(" "))
+  }
+  const handleCopy = () => {
+console.log("I  am copy")
+var text = document.getElementById("myBox")
+text.select();
+text.setSelectionRange(0, 9999)
+navigator.clipboard.writeText(text.value);
+  }
   const [text, setText] = useState("");
 
   return (
@@ -38,7 +49,13 @@ export default function TextForm(props) {
           Convert to Lowercase
         </button>
         <button className="btn btn-primary mx-1" onClick={handleClearClick}>
-         Clear Text
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+          handleExtraSpaces
         </button>
       </div>
       <div className="container my-3">
